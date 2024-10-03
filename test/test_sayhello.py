fba64284e646f3b319c7ec4264c15de30193ea1f
ChawanwitDdata <chawanwit@data.co.th>
2024-10-03 14:22:09 +0700

    [Update] add test_sayhello

---
+++ test/test_sayhello.py
@@ -0,0 +1,16 @@
+#--- Say Hello
+def say_hello(name):
+    return f"Hello, {name}!"
+#--- Say Hi
+def hi(name):
+    return f"Hi, {name}!"
+#--- Say bye ---
+def bye(name):
+    return f"Bye, {name}!"
+#--- Say name ---
+def sayname(name):
+    return f"Ohayo {name}!!!!!"
+#--- Say message ---
+def sayit(message):
+    return f"{message}"
+
